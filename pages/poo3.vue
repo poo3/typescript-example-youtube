4e57e8af5aa23140c730e72a1e0b2bcf2c5f740e
poo3fuma <coolishpoo3@gmail.com>
2022-02-13 12:28:01 +0900

    Create single component

---
+++ pages/poo3.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <h3>{{ fullName }}</h3>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+interface User {
+  firstName: string;
+  lastName: string;
+}
+
+export default Vue.extend({
+  data() {
+    return {
+      message: "Hello World!!",
+      firstName: "poo3",
+      lastName: "fuma",
+    };
+  },
+  computed: {
+    fullName(): string {
+      return this.firstName + this.lastName;
+    },
+  },
+});
+</script>
+
+<style scoped></style>
